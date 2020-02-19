6067cf88eb5294a88f90fe48da082c2e85245353
Ben Clayton-Green <ben-clayton-green@Bens-MacBook-Air.local>
2020-02-19 19:41:29 +0000

    Extra randomness to repaint & blank canvas option

--- script.js
+++ script.js
@@ -3,22 +3,23 @@
 
 // Options:
 // --  1. canvas building 
-const numOfElements = 2000
+const elementCreationAttempts = 2000
 const placementAttempts = 20
 const divSize = 2
 const spaceBetween = 2
 const margin = 40
 const originalBandwidth = 100
+const blankCanvas = false
 
 // -- 2. onMouseMove
 const repaintSlowness = 9
 const brushRadius = 20
 const brushTracking = 50
 
-// working variables
+// operational variables
 const windowWidth = window.innerWidth
 const windowHeight = window.innerHeight
-let gridPositions = []
+let GridPositions = []
 let bandwidth = originalBandwidth
 let clickAllowed = true
 let firstTime = true
@@ -27,26 +28,28 @@ MakeCanvasWithCircles();
 
 document.addEventListener('mousemove', function(e){
     if(firstTime){
-        repaintAtSlowness(1,0,e)
+        repaintRainbow(1,0,e)
         firstTime = false
     }
-
+    
     const rand = Math.floor(Math.random()*repaintSlowness)
-    repaintAtSlowness(repaintSlowness, rand, e)
+    const rand2 = Math.round(Math.random()-0.3 * 4) + repaintSlowness
+    repaintRainbow(rand2, rand, e)
 
     if (e.buttons != 0) {
         if (clickAllowed){
             bandwidth = originalBandwidth*1.5
             clickAllowed = false
             setTimeout(() => clickAllowed = true, brushTracking)
-            for (let i = 0; i < gridPositions.length; i ++) {
-                const pos = gridPositions[i]
+            for (let i = 0; i < GridPositions.length; i ++) {
+                const pos = GridPositions[i]
                 const distance = findDistance({left: e.x, top: e.y}, pos)
                 if (distance  <= brushRadius + pos.size/3){
                     const elem = document.getElementById(pos.id)
-                    elem.classList.add('drawnOn')
-                    if(e.shiftKey){
+                    if (blankCanvas ? !e.shiftKey : e.shiftKey){
                         elem.classList.remove('drawnOn')
+                    }else{
+                        elem.classList.add('drawnOn')
                     }
                 }
             }
@@ -55,19 +58,19 @@ document.addEventListener('mousemove', function(e){
     else {bandwidth = originalBandwidth}
 })
 
-function repaintAtSlowness(slowness, startPoint, cursor){
-    for (let i = startPoint; i < gridPositions.length; i += slowness) {
-        var pos = gridPositions[i]
-        var distance = findDistance({left: cursor.x, top: cursor.y}, pos) 
+function repaintRainbow(slowness, startPoint, cursor){
+    for (let i = startPoint; i < GridPositions.length; i += slowness) {
+        const pos = GridPositions[i]
+        const distance = findDistance({left: cursor.x, top: cursor.y}, pos) 
             + (Math.random()-0.5)*100
-        var factor = Math.floor(distance / bandwidth) - 1
+        const factor = Math.floor(distance / bandwidth) - 1
         document.getElementById(pos.id).setAttribute('band','band' + factor)
     }
 }
 
 function findDistance(pos1, pos2){
-    var xDist = Math.abs(pos1.left - pos2.left)
-    var yDist = Math.abs(pos1.top - pos2.top)
+    const xDist = Math.abs(pos1.left - pos2.left)
+    const yDist = Math.abs(pos1.top - pos2.top)
     return Math.sqrt(xDist*xDist + yDist*yDist)
 }
 
@@ -76,17 +79,17 @@ function findDistance(pos1, pos2){
 
 // inital circles placement functions till end of file
 function MakeCanvasWithCircles() {
-    var main = document.getElementById("main")
-    for (let i = 0; i < numOfElements; i++) {
+    const main = document.getElementById("main")
+    for (let i = 0; i < elementCreationAttempts; i++) {
     
-        var divPosition = createCircleFindSpace()
+        const divPosition = createCircleFindSpace()
         if (divPosition) {
             id = `circle${i}`
-            gridPositions.push({...divPosition, id})
-            var elem = document.createElement("div")
-            elem.classList.add('myClass')
+            GridPositions.push({...divPosition, id})
+            const elem = document.createElement("div")
+            elem.classList.add('circle', blankCanvas && 'drawnOn')
             elem.id = id
-            var style = `top: ${divPosition.top}px;
+            const style = `top: ${divPosition.top}px;
                 left: ${divPosition.left}px;
                 width: ${divPosition.size}px;
                 height: ${divPosition.size}px;
@@ -95,12 +98,13 @@ function MakeCanvasWithCircles() {
             main.appendChild(elem)
         }
     }
-    console.log(gridPositions);
+
+    console.log(GridPositions);
 }
 
 function createCircleFindSpace() {
     for (let i = placementAttempts; i > 0; i--) {
-        var position = createRandomPosition(i/placementAttempts)
+        const position = createRandomPosition(i/placementAttempts)
         if (divFits(position) && !divTouchesEdge(position)){
             return position
         }
@@ -109,10 +113,10 @@ function createCircleFindSpace() {
 }
 
 function createRandomPosition (sizeFactor) {
-    var size = Math.random() * (sizeFactor * divSize * 25) + 10
-    var top = Math.random() * (windowHeight - margin*2) + margin
+    const size = Math.random() * (sizeFactor * divSize * 25) + 10
+    let top = Math.random() * (windowHeight - margin*2) + margin
     top += (Math.random() - 0.5) * windowHeight / margin
-    var left = Math.random() * (windowWidth - margin*2) + margin
+    let left = Math.random() * (windowWidth - margin*2) + margin
     left += (Math.random() - 0.5) * windowWidth / margin
     
     return {
@@ -123,20 +127,16 @@ function createRandomPosition (sizeFactor) {
 }
 
 function divFits (divPosition) {
-    if (gridPositions.length == 0){
-        return true
-    }
-    var itDoesntFit = gridPositions.some((position) => {
-        var distance = findDistance(divPosition, position)
-        if (distance - spaceBetween <= divPosition.size/2 + position.size/2){
+    return GridPositions.every((position) => {
+        const distance = findDistance(divPosition, position)
+        if (distance - spaceBetween >= divPosition.size/2 + position.size/2){
             return true
         }
     })
-    return !itDoesntFit
 }
 
 function divTouchesEdge(pos){
-    var rand = (Math.random() - 0.5) * 5000
+    const rand = (Math.random() - 0.5) * 5000
     if(
         pos.top < margin + pos.size/2 + rand ||
         pos.top > windowHeight - margin - pos.size/2 + rand ||
